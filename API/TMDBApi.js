284993ac087f4ed330fbd3390c6c5edd049ed9fb
Crow <tan.beauoudong@gmail.com>
2018-06-03 21:07:15 +0200

    update

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,7 +1,7 @@
 const API_TOKEN = "863d189692081dd1daf38b7bf7512216"
 
-export function getMoviesFromQuery (text) {
-	const url = 'https://api.themoviedb.org/3/search/movie/?api_key=' + API_TOKEN + '&language=fr&query=' + text
+export function getMoviesFromQuery (text, page) {
+	const url = 'https://api.themoviedb.org/3/search/movie/?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
 	return fetch(url)
 		.then((res) => res.json())
 }
--- Components/Search.js
+++ Components/Search.js
@@ -21,6 +21,8 @@ class Search extends Component {
 	constructor(props) {
 	  	super(props);
   		this.searchedText =  ''
+  		this.page = 0
+  		this.totalPages = 0
 	  	this.state = { 
 	  		films: [],
 	  		isLoading: false
@@ -35,15 +37,27 @@ class Search extends Component {
 		console.log(this.searchedText);
 		if (this.searchedText.length > 0) {
 			this.setState({ isLoading: true })
-	  		getMoviesFromQuery(this.searchedText)
+	  		getMoviesFromQuery(this.searchedText, this.page + 1)
 	  			.then((data) => {
+	  				this.page = data.page
+	  				this.totalPages = data.total_pages
 	  				this.setState({ 
-	  					films: data.results,
+	  					films: [ ...this.state.films, ...data.results ], // this.state.films.concat(data.results)
 	  					isLoading: false
 					})
 				})
   		}
   	}
+
+  	_searchFilms() {
+  		this.page = 0
+  		this.totalPages = 0
+  		this.setState({
+  			films: []
+  		})
+ 		console.log("Page : " + this.page + " / TotalPages : " + this.totalPages + " / Nombre de films : " + this.state.films.length)
+   		this._loadFilms()
+  	} 
 
   	_displayLoading() {
   		if (this.state.isLoading) {
@@ -61,13 +75,20 @@ class Search extends Component {
 			<View style={styles.main_container}>
 	  			<TextInput placeholder="Titre du film" 
 	  				onChangeText={(text) => this._searchTextInputChanged(text)} 
-	  				onSubmitEditing={(text) => this._loadFilms()} 
+	  				onSubmitEditing={(text) => this._searchFilms()} 
 	  				style={[styles.textinput, { marginBottom: 10 }]}/>
-	  			<Button title="Rechercher" onPress={() => { this._loadFilms() }} style={styles.button}/>
+	  			<Button title="Rechercher" onPress={() => { this._searchFilms() }} style={styles.button}/>
 	  			<FlatList
 	  				data={this.state.films}
 	  				keyExtractor={(item) => item.id.toString()}
-	  				renderItem={({item}) => <FilmItem film={item}/>}
+	  				renderItem={({item}) => <FilmItem film={item}/> }
+	  				onEndReachedThreshold={0.5}
+	  				onEndReached={() => {
+	  					if (this.state.films.length > 0 && this.page < this.totalPages) {
+	  						this._loadFilms()
+	  					}
+	  					console.log('onEndReached') 
+	  				}}
 	  			/>
 	  			{this._displayLoading()}
 	  		</View>
